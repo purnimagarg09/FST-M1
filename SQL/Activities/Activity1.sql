65fe787d11fe77c4ba9371483b2602aa6f72729d
purnimagarg09 <53164420+purnimagarg09@users.noreply.github.com>
2022-05-27 09:26:47 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,12 @@
+REM   Script: Activity1
+REM   Activity1 SQL
+
+CREATE TABLE salesman( 
+salesman_id int primary key, 
+salesman_name varchar2(20), 
+salesman_city varchar2(20), 
+commission int);
+
+DESCRIBE salesman;
+
+
---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,10 @@
+REM   Script: Activity4
+REM   Activity4 SQL
+
+ALTER TABLE salesman  
+ADD grade int;
+
+UPDATE salesman set grade=100;
+
+select * from salesman;
+
